8513efe6caa3aca760fe3a677ebc0d43c72c0ff9
John Baldwin <johnbaldwin82@gmail.com>
2023-08-02 14:51:46 -0600

    myresume added

---
+++ src/components/MyResume/index.js
@@ -0,0 +1,31 @@
+import React from "react"
+function MyResume() {
+    return(
+        <section className='my-5'>
+            <div classname= 'my-2'>
+                <h3>Fulle Stack Developer Skills</h3>
+                <ul className="talents">
+                    <li>React</li>
+                    <li>JavaScripts</li>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>Bootstrap</li>
+                    <li>jQuery</li>
+                    <li>MySQL</li>
+                    <li>GraphQL</li>
+                    <li>Express</li>
+                    <li>APIS</li>
+                    <li>MongoDB</li>
+                    <li>Node.js</li>
+                    <li></li>
+                </ul>
+                <p>
+                    Please visit my Repo to see my work <a href="https://github.com/JohnBaldwin82">My Resume</a>
+                </p>
+            </div>
+        </section>
+        
+    )
+}
+
+export default MyResume
