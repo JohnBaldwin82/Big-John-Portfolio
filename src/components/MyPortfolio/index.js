f6a57552641b7e96815c081aa04076e375d8dbf9
John Baldwin <johnbaldwin82@gmail.com>
2023-08-02 09:57:52 -0600

    more coponents and screenshots

---
+++ src/components/MyPortfolio/index.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import Projects from "../Projects";
+
+function MyPortfolio() {
+    const [projects] = useState([
+  
+    {
+      name: "Employee-Tracker",
+      description: "Node, Inquirer, MySQL",
+      repo: "https://github.com/JohnBaldwin82/Employee-Tracker",
+    },
+    {
+      name: "Chat-Camp",
+      description: "Node, Express, MySql, Socket-IO",
+      repo: "https://github.com/JohnBaldwin82/Chat-Camp",
+      link: "https://chat-camp.herokuapp.com/",
+    },
+    {
+      name: "Food(d)-And-Bar",
+      description: "CSS, Server-Side API's, java script",
+      repo: "https://github.com/JohnBaldwin82/Food-And-Recipe-Generator",
+    },
+    {
+        name: "Note-Taker",
+        description: "Express",
+        repo: "https://github.com/JohnBaldwin82/Note-Taker",
+    },
+    {
+        name: "Text-Editor",
+        description: "PWA, ",
+        repo: "https://github.com/JohnBaldwin82/Text-Editor",
+    },
+  ]);
+
+  return (
+    <div className="MyPortfolio">
+        <div className="flex-column">
+        <div className="card">
+            {Projects.map((project, idx) => (
+                <Project
+                project={project}
+                key={"project" + idx}
+                />
+            ))}
+            </div>
+        </div>
+    </div>
+)
+}
+
+export default MyPortfolio
+
+
+            
